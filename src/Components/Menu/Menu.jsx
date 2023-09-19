f8a1ad6d9ac733c523fff16b2f1dc85d473d2ef5
Your Name <pradeeprawat1717@gmail.com>
2023-09-19 16:21:50 +0530

    Innitial Commit

--- src/Components/Menu/Menu.jsx
+++ src/Components/Menu/Menu.jsx
@@ -5,9 +5,10 @@ import { Link } from 'react-router-dom'
 import { Container, Row, Col, Card, Image} from 'react-bootstrap'
 import {LiaLessThanSolid} from 'react-icons/lia'
 import menuList from '../../data/menu'
+import axios from 'axios'
 
 const Menu = () => {
-  
+
   // Initialize the array in state, or load it from localStorage if available
   const [myArray, setMyArray] = useState(() => {
     const storedArray = localStorage.getItem('myArray');
@@ -68,6 +69,7 @@ const Menu = () => {
 
   //   setAmount(amount)
   // }
+  
   return (
     <>
       <Container className='menuContainer text-light bg_Dark p-0 d-xs-flex' fluid> 
@@ -77,47 +79,10 @@ const Menu = () => {
               <LiaLessThanSolid size={20} />
             </div>
           </Link>
-          
           <div>
             <h4 className='m-0'>Digital Menu</h4>
           </div>
         </div>
-        {/* <Row className='p-0 m-0 mt-3 px-2'>
-        {menuList.map((menuList, index) => (
-          <Col className='p-2' xs={12} sm={6} md={4} lg={3} xl={2} key={index}>
-            <Card className='bg-dark text-light'>
-            <Card.Img className='' variant="top" src={menuList.picture} />
-              <Card.Body>
-                <Card.Title>Caprese Salad With Pasto Sauce</Card.Title>
-                <span className=''>€{menuList.price}</span>
-                <Card.Text className=''>
-                  Low carb, low calorie, keto
-                </Card.Text>
-                {
-                  ((myArray[index]===0)
-                      ? 
-                      (<div className='bg_Success countItemBtn text-light text-center' onClick={() => incrementAtIndex(index, menuList.price)}>
-                        +
-                      </div>)
-                      : 
-                    (<div className='countItemBtn d-flex justify-content-evenly'>
-                      <div className='minusBtn' onClick={() => decrementAtIndex(index)} >
-                        -
-                      </div>
-                      <div>
-                        {myArray[index]}
-                      </div>
-                      <div onClick={() => incrementAtIndex(index, menuList.price)}>
-                        +
-                      </div>
-                  </div>)  
-                  )
-                }
-              </Card.Body>
-            </Card>
-          </Col>
-        ))}
-        </Row> */}
         <Row className='p-0 m-0 mt-3 px-2'>
         {menuList.map((menuList, index) => (
           <Col key={index} className='p-2 ' xs={12} sm={6} md={4} lg={3} xl={2}>
