d4995142ee69c75d30679f29af594799bb7180ab
Pawan Sen <pawan@vebuin.com>
2023-09-22 12:02:31 +0100

    update

--- src/Components/Menu/Menu.jsx
+++ src/Components/Menu/Menu.jsx
@@ -1,19 +1,25 @@
-import {React, useState, useEffect} from 'react'
+import { React, useState, useEffect } from 'react'
+import { useNavigate } from 'react-router-dom';
 import './Menu.css'
 import '../../index.css'
 import { Link } from 'react-router-dom'
-import { Container, Row, Col, Image} from 'react-bootstrap'
-import {LiaLessThanSolid} from 'react-icons/lia'
+import { Container, Row, Col, Image } from 'react-bootstrap'
+import { LiaLessThanSolid } from 'react-icons/lia'
 import axios from 'axios'
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
-const Menu = () => {
+const Menu = (props) => {
 
   const [itemCount, setItemCount] = useState([])
   const [loading, setLoading] = useState(true)
   const [data, setData] = useState([])
+  const navigate = useNavigate();
+  const queryParameters = new URLSearchParams(window.location.search)
+  const assetId = queryParameters.get("assetId")
 
   const fetchData = async () => {
-    const response = await fetch('http://194.163.149.48:3002/admin/menu/get-menu')
+    const response = await fetch('http://194.163.149.48:3002/admin/menu/get-menu?pageNo=1&size=10')
     if (!response.ok) {
       throw new Error('Data coud not be fetched!')
     } else {
@@ -31,14 +37,14 @@ const Menu = () => {
         setLoading(true)
       })
 
-      const storedArray = localStorage.getItem('localArray');
-      if (storedArray) {
-        setItemCount(JSON.parse(storedArray));
-      }
+    const storedArray = localStorage.getItem('localArray');
+    if (storedArray) {
+      setItemCount(JSON.parse(storedArray));
+    }
   }, [])
 
 
-  if(data.length && !itemCount.length){
+  if (data.length && !itemCount.length) {
     setItemCount(Array(data.length).fill(0));
   }
 
@@ -63,8 +69,8 @@ const Menu = () => {
     localStorage.setItem('localArray', JSON.stringify(updatedArray));
   };
 
-   // Function to delete the stored array from localStorage
-   const cancelOrder = () => {
+  // Function to delete the stored array from localStorage
+  const cancelOrder = () => {
     clearItems();
     localStorage.removeItem('localArray');
   };
@@ -72,8 +78,8 @@ const Menu = () => {
   const items = [];
   function pushSelectedItems() {
     itemCount.map(function (count, i) {
-      if(itemCount[i]>0){
-        const item = {menuId:`${data[i]._id}`,quantity:parseInt(`${itemCount[i]}`)}
+      if (itemCount[i] > 0) {
+        const item = { menuId: `${data[i]._id}`, quantity: parseInt(`${itemCount[i]}`) }
         items.push(item)
       }
     });
@@ -82,7 +88,7 @@ const Menu = () => {
   function callAxiosAPI() {
 
     let dataApi = JSON.stringify({
-      "assetId": `${localStorage.getItem('assetId')}`,
+      "assetId": `${assetId}`,
       "menu": items
     });
 
@@ -98,11 +104,17 @@ const Menu = () => {
 
     axios.request(config)
       .then((response) => {
-        console.log(JSON.stringify(response.data));
+        toast.success('Success !', {
+          position: toast.POSITION.TOP_RIGHT
+        });
+        navigate(`/orderPlaced?assetId=` + assetId);
       })
       .catch((error) => {
         console.log(error);
-      });
+        toast.error('Invalid Asset !', {
+          position: toast.POSITION.TOP_RIGHT
+        });
+      });
   }
 
   const placeOrder = () => {
@@ -113,7 +125,7 @@ const Menu = () => {
 
   return (
     <>
-      <Container className='menuContainer text-light bg_Dark p-0 d-xs-flex' fluid> 
+      <Container className='menuContainer text-light bg_Dark p-0 d-xs-flex' fluid>
         <div className='d-flex text-light align-items-center py-2'>
           <Link to="/" className='text-light'>
             <div className='menuLogo d-flex align-items-center mx-3 p-2'>
@@ -121,64 +133,75 @@ const Menu = () => {
             </div>
           </Link>
           <div>
-            <h4 className='m-0'>Digital Menu</h4>
+            <h4 className='m-0'>Room Eats Menu</h4>
           </div>
         </div>
-
-        { 
-        loading 
-        ? 
-          <h3 className='text-center mt-5'>Loading...</h3> 
-        :
-        <>
-        <Row className='cardContainer p-0 m-0 mt-3 px-2'>
-        {data.slice(0,5).map((data, index) => (
-          <Col key={index} className='p-2 ' xs={12} sm={6} md={4} lg={3} xl={2}>
-            <Row className='cardDetailContainer h-100 rounded bg_LightDark p-0 m-0 d-flex justify-content-center align-items-center'>
-              <Col className='m-0 p-0 d-flex justify-content-center align-items-center' xs={3} sm={12}>
-                <div className='imageContainer'>
-                  <Image className='cardImage w-100 h-100' src="https://howtostartanllc.com/images/business-ideas/business-idea-images/fast-food.jpg"/>
-                </div>
-              </Col>
-              <Col className='itemDetailContainer' xs={6} sm={12}>
-                  <h5 className='p-0 m-0'>{(`${data.name}`).toLowerCase()}</h5>
-                  <p className='p-0 m-0'>€{(`${data.price}`).toLowerCase()}</p>
-                  <p className='p-0 m-0 colorLightGray'>{(`${data.description}`).toLowerCase()}</p>
-              </Col>
-              <Col className='py-3' xs={3} sm={12}>
-                {
-                  ((itemCount[index]===0)
-                      ? 
-                      (<div className='bg_Success countItemBtn text-light text-center' onClick={() => incrementAtIndex(index, data.price)}>
-                        +
-                      </div>)
-                      : 
-                    (<div className='countItemBtn d-flex justify-content-evenly'>
-                      <div className='minusBtn' onClick={() => decrementAtIndex(index)} >
-                        -
-                      </div>
-                      <div>
-                        {itemCount[index]}
-                      </div>
-                      <div onClick={() => incrementAtIndex(index, data.price)}>
-                        +
-                      </div>
-                  </div>)  
-                  )
-                }
-              </Col>
-            </Row>
-          </Col>
-        ))}
-        </Row>
-        <div className='d-flex justify-content-center py-5' varient="bottom">
-          <Link to="/"> <button onClick={()=> cancelOrder()} className='bg-danger border-danger countItemBtn text-light text-center'>Cancel Order</button> </Link>
-          <button onClick={()=> clearItems()} className='bg-primary countItemBtn text-light border-primary text-center mx-3'>Clear Items</button>
-          <Link to="/orderPlaced"> <button onClick={placeOrder} className='bg_Success countItemBtn text-light text-center'>Place Order</button> </Link>
-        </div>
-      </>
+        <ToastContainer
+          position='top-right'
+          autoClose={5000}
+          hideProgressBar={false}
+          newestOnTop={false}
+          closeOnClick
+          rtl={false}
+          pauseOnFocusLoss
+          draggable
+          pauseOnHover
+          theme='light'
+        />
+        {
+          loading
+            ?
+            <h3 className='text-center mt-5'>Loading...</h3>
+            :
+            <>
+              <Row className='cardContainer p-0 m-0 mt-3 px-2'>
+                {data.map((data, index) => (
+                  <Col key={index} className='p-2 ' xs={12} sm={6} md={4} lg={3} xl={2}>
+                    <Row className='cardDetailContainer h-100 rounded bg_LightDark p-0 m-0 d-flex justify-content-center align-items-center'>
+                      <Col className='m-0 p-0 d-flex justify-content-center align-items-center' xs={3} sm={12}>
+                        <div className='imageContainer'>
+                          <Image className='cardImage w-100 h-100' src="https://howtostartanllc.com/images/business-ideas/business-idea-images/fast-food.jpg" />
+                        </div>
+                      </Col>
+                      <Col className='itemDetailContainer' xs={6} sm={12}>
+                        <h5 className='p-0 m-0'>{(`${data.name}`).toLowerCase()}</h5>
+                        <p className='p-0 m-0'>€{(`${data.price}`).toLowerCase()}</p>
+                        <p className='p-0 m-0 colorLightGray'>{(`${data.description}`).toLowerCase()}</p>
+                      </Col>
+                      <Col className='py-3' xs={3} sm={12}>
+                        {
+                          ((itemCount[index] === 0)
+                            ?
+                            (<div className='bg_Success countItemBtn text-light text-center' onClick={() => incrementAtIndex(index, data.price)}>
+                              +
+                            </div>)
+                            :
+                            (<div className='countItemBtn d-flex justify-content-evenly'>
+                              <div className='minusBtn' onClick={() => decrementAtIndex(index)} >
+                                -
+                              </div>
+                              <div>
+                                {itemCount[index]}
+                              </div>
+                              <div onClick={() => incrementAtIndex(index, data.price)}>
+                                +
+                              </div>
+                            </div>)
+                          )
+                        }
+                      </Col>
+                    </Row>
+                  </Col>
+                ))}
+              </Row>
+              <div className='d-flex justify-content-center py-5' varient="bottom">
+                <Link to="/"> <button onClick={() => cancelOrder()} className='bg-danger border-danger countItemBtn text-light text-center'>Cancel Order</button> </Link>
+                <button onClick={() => clearItems()} className='bg-primary countItemBtn text-light border-primary text-center mx-3'>Clear Items</button>
+                <Link > <button onClick={placeOrder} className='bg_Success countItemBtn text-light text-center'> Place Order</button> </Link>
+              </div>
+            </>
         }
-      </Container>
+      </Container >
     </>
   )
 }
--- src/Components/OrderPlaced/OrderPlaced.jsx
+++ src/Components/OrderPlaced/OrderPlaced.jsx
@@ -2,18 +2,27 @@ import React from 'react'
 import './OrderPlaced.css'
 import '../../index.css'
 import { Container } from 'react-bootstrap'
-import {BsCheckLg} from 'react-icons/bs'
+import { BsCheckLg } from 'react-icons/bs'
 import { Link } from 'react-router-dom'
 
 const OrderPlaced = () => {
+
+  const ListItemTextWithDetails = () => {
+
+    const queryParameters = new URLSearchParams(window.location.search)
+    const assetId = '/menu?assetId=' + queryParameters.get("assetId")
+
+    return <Link to={assetId} > <button className='primaryButton mt-5'>Add More Orders</button> </Link >
+  }
+
   return (
     <>
       <Container className='orderPlacedContainer d-flex justify-content-center align-items-center text-light'>
         <div className='orderPlacedDetails text-center'>
-            <BsCheckLg size={60} className='borderRounded bg_Success p-1 m-3' />
-            <h5>Order Placed Successfully</h5>
-            <p className='colorLightGray'>Your Order has been placed. You can view order details from my account section.</p>
-            <Link to="/menu"> <button className='primaryButton mt-5'>Add More Orders</button> </Link>
+          <BsCheckLg size={60} className='borderRounded bg_Success p-1 m-3' />
+          <h5>Order Placed Successfully</h5>
+          <p className='colorLightGray'>Your Order has been placed. You can view order details from my account section.</p>
+          <ListItemTextWithDetails />
         </div>
       </Container>
     </>
